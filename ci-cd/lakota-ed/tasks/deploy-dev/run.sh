ae3d06315f5c9af0c5134653e33ae5bdf06fa25e
Sunil <srao0194@protonmail.com>
2019-08-01 23:52:43 -0500

    fixed rsa key

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -18,6 +18,6 @@ cd dev-lakota-ed
 npm install
 npm run build
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r build/* root@167.71.81.111:/var/www/html
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp build/ root@167.71.81.111:/var/www/html
 
 rm ~/.ssh/id_rsa
