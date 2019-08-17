5c2450437a5ab688b57d003df092bbb56644fd9c
Sunil <srao0194@protonmail.com>
2019-08-17 02:27:03 -0500

    Fixed basic auth responses, removed unnecessary bodies

--- lakota_ed_api/lib/lakota_ed_api_web/controllers/user_controller.ex
+++ lakota_ed_api/lib/lakota_ed_api_web/controllers/user_controller.ex
@@ -7,15 +7,15 @@ defmodule LakotaEdApiWeb.UserController do
 
   def index(conn, %{"email" => email, "password" => password}) do
     case Repo.get_by(LakotaEdApi.Accounts.User, email: email) do
-      nil -> render(conn, "login_error.json", email: email) # User does not exist
+      nil -> send_resp(conn, 404, "User with that e-mail not found.")
       user -> authenticate_user(conn, password, user)
     end
   end
 
   def authenticate_user(conn, inputPassword, realUser = %LakotaEdApi.Accounts.User{}) do
     case Bcrypt.verify_pass(inputPassword, realUser.encrypted_password) do
-      false -> render(conn, "login_error.json", email: realUser.email) # Incorrect password for associated e-mail
-      true -> render(conn, "login_success.json", email: realUser.email)
+      false -> send_resp(conn, 401, "Incorrect password for e-mail.")
+      true -> send_resp(conn, 200, "Successfully logged in.")
     end
   end
 end
