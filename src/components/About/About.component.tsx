3e7089dd211938b561ab98b772d768ec2f04c945
Sunil <srao0194@protonmail.com>
2020-05-17 17:29:48 -0500

    updated About section

--- src/components/About/About.component.tsx
+++ src/components/About/About.component.tsx
@@ -14,7 +14,10 @@ export const About: FC = () => {
                 need to learn Lakȟótiyapi, a language that nobody in my family speaks, due to the attendance of my
                 grandmother’s grandmother at the Benedictine Agricultural Boarding School in Kenel. I’ve been learning
                 the language for a couple years now.</p>
+            <p>If you have any questions or would like to discuss anything, please reach out to me via <a href="mailto:lakotalanguagelearning@gmail.com">lakotalanguagelearning@gmail.com</a>
+                </p>
             <b>Social Media:</b>
+            <br/>
             <br/>
             <div className="buttons">
                 <a href='https://twitter.com/LanguageLakota'
