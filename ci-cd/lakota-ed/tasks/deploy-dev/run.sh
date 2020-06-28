3bf43fbe28adf6c0f3129968e3250ad073a004c1
Sunil <srao0194@protonmail.com>
2020-06-28 15:21:57 -0500

    update integration test nodejs image

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -21,4 +21,4 @@ npm install
 npm run build
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@skeletonpraxis.net "rm -rfv /var/www/lakota.skeletonpraxis.net/*"
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp build/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r build/* root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
--- ci-cd/lakota-ed/tasks/integration-test/run.sh
+++ ci-cd/lakota-ed/tasks/integration-test/run.sh
@@ -7,7 +7,7 @@ cd dev-lakota-ed
 # This is where you can run your unit test command, such as the ones below:
 echo "Running integration tests against QA..."
 
-apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+apt-get install libgdk-pixbuf2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 
 npm install
 npm run integration-test:qa
