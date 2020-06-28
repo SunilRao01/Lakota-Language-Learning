1bfeb570a1a8ab532ad6418e82f9c542173c9369
Sunil <srao0194@protonmail.com>
2020-06-28 04:09:00 -0500

    update deployment shell script

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -9,16 +9,15 @@ set -e -u -x
     mkdir /root/.ssh
     cp ./rsa.key ~/.ssh/id_rsa
     ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
+    ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
     rm rsa.key
     rm rsa.key.pub
 }
-
-
 
 cd dev-lakota-ed
 npm install
 npm run build
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp build/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
+scp -rp build/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
 
 rm ~/.ssh/id_rsa
