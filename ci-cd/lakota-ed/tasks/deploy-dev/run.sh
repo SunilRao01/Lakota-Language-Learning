ae4f84224ff360dd103a239b63f84bd96f28adb1
Sunil <srao0194@protonmail.com>
2020-06-28 13:40:23 -0500

    fix conflicts

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -2,30 +2,31 @@
 
 set -e -u -x
 
-#{
-#  mkdir -p /root/.ssh
-#  chmod 0700 ~/.ssh
+{
+  mkdir -p /root/.ssh
+  chmod 0700 ~/.ssh
 #
-#  ln -s /run/secrets/private_rsa_key /root/.ssh/id_rsa
-#  ln -s /run/secrets/public_rsa_key /root/.ssh/id_rsa.pub
+  ln -s /run/secrets/private_rsa_key /root/.ssh/id_rsa
+  ln -s /run/secrets/public_rsa_key /root/.ssh/id_rsa.pub
 
 #  cat ~/.ssh/id_rsa
 #  cat ~/.ssh/id_rsa.pub
 #
-#  touch /root/.ssh/known_hosts
-#  chmod 644 ~/.ssh/known_hosts
+  touch /root/.ssh/known_hosts
+  chmod 644 ~/.ssh/known_hosts
 #
-#  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
+  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
 #  ssh-keyscan -H 134.122.124.158 >> /root/.ssh/known_hosts
 
 #  cat /run/secrets/private_rsa_key
 #  cat /run/secrets/public_rsa_key
-#}
+}
 ls ~/.ssh
 cd dev-lakota-ed
 #npm install
 #npm run build
 
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp public/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
+sftp root@skeletonpraxis.net
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp public/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
 
 rm ~/.ssh/id_ed25519
