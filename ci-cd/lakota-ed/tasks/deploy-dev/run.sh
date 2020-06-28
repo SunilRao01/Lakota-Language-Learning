85f3d41c5c8d7ad6940ba9fb29a3c54121a3223d
Sunil <srao0194@protonmail.com>
2020-06-28 15:31:45 -0500

    fixed staging build for dev deployment

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -5,6 +5,7 @@ set -e -u -x
 {
   private_rsa_key=${PRIVATE_RSA_KEY}
   public_rsa_key=${PUBLIC_RSA_KEY}
+  env_staging=${ENV_STAGING}
 
   ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
   echo "$private_rsa_key" > ~/.ssh/id_rsa
@@ -17,6 +18,9 @@ set -e -u -x
 }
 
 cd dev-lakota-ed
+
+touch .env.staging
+echo "$env_staging" > .env.staging
 npm install
 npm run build:staging
 
