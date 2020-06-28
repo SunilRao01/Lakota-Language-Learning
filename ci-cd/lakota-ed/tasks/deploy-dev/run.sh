ce764a643e37df33a0703635e949e11aadada641
Sunil <srao0194@protonmail.com>
2020-06-28 05:50:44 -0500

    test dev

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -4,17 +4,21 @@ set -e -u -x
 
 {
     rsa=${RSA_KEY}
-    ssh-keygen -t rsa -N "" -f rsa.key
-    echo "$rsa" > ./rsa.key
-    mkdir /root/.ssh
-    cp ./rsa.key ~/.ssh/id_rsa
-    rm rsa.key
-    rm rsa.key.pub
-
-    touch ~/.ssh/id_rsa.pub
-    chmod 644 ~/.ssh/id_rsa.pub
-
     ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+    ls ~/.ssh/
+#    echo
+#
+#    ssh-keygen -t rsa -N "" -f rsa.key
+#    echo "$rsa" > ./rsa.key
+#    mkdir /root/.ssh
+#    cp ./rsa.key ~/.ssh/id_rsa
+#    rm rsa.key
+#    rm rsa.key.pub
+#
+#    touch ~/.ssh/id_rsa.pub
+#    chmod 644 ~/.ssh/id_rsa.pub
+#
+#    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 
     ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
     ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
