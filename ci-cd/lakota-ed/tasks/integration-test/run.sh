de104ac5b6938b29d023ce7aaebd77cd3438bf6e
Sunil <srao0194@protonmail.com>
2020-06-28 15:33:44 -0500

    fixed staging build for dev deployment

--- ci-cd/lakota-ed/tasks/integration-test/run.sh
+++ ci-cd/lakota-ed/tasks/integration-test/run.sh
@@ -7,8 +7,6 @@ cd dev-lakota-ed
 # This is where you can run your unit test command, such as the ones below:
 echo "Running integration tests against QA..."
 
-apt-get install libgdk-pixbuf2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
-
 npm install cypress --save-dev
 CYPRESS_BASE_URL=http://lakota.skeletonpraxis.net/ cypress run
 
