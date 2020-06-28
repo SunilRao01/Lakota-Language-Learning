225023f619a5ff2e284077ccc1657dd5f9b6b8c8
Sunil <srao0194@protonmail.com>
2020-06-28 07:33:07 -0500

    test dev

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -30,6 +30,6 @@ cd dev-lakota-ed
 #npm install
 #npm run build
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp build/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -rp public/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
 
 rm ~/.ssh/id_rsa
