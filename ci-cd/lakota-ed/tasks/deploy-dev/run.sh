1e8c4f7d1739772efa9213264e8d0196fd07cfc4
Sunil <srao0194@protonmail.com>
2020-06-28 05:04:18 -0500

    test dev

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -3,13 +3,12 @@
 set -e -u -x
 
 {
-    ssh-keygen -f ~/.ssh/id_rsa -b 4096 -t rsa -q -N ""
-
     rsa=${RSA_KEY}
     echo "$rsa" > ./rsa_key
     chmod 600 ./rsa_key
 
-    ssh-keygen -f ./rsa_key -b 4096 -t rsa -q -N "" > ~/.ssh/id_rsa.pub
+    ssh-keygen -f ./rsa_key -b 4096 -t rsa -q -N ""
+    ls
     ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
     ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
     pwd
