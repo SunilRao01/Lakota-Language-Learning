7f49491070544c7772a0a91860bd72d66d418e43
Sunil <srao0194@protonmail.com>
2020-06-28 06:28:44 -0500

    test dev

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -6,11 +6,13 @@ set -e -u -x
   mkdir /root/.ssh
   chmod 700 ~/.ssh
 
-  rsa_private=${PRIVATE_RSA_KEY}
   rsa_public=${PUBLIC_RSA_KEY}
+  rsa_private=${PRIVATE_RSA_KEY}
 
   echo "$rsa_private" > ~/.ssh/id_rsa
   echo "$rsa_public" > ~/.ssh/id_rsa.pub
+
+  ls ~/.ssh
 
   ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
   ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
@@ -20,6 +22,6 @@ cd dev-lakota-ed
 #npm install
 #npm run build
 
-scp -rp public/ root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
+scp -r public/* root@skeletonpraxis.net:/var/www/lakota.skeletonpraxis.net
 
 rm ~/.ssh/id_rsa
