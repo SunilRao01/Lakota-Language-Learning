2c29afbbaee70b38b27ad85367950192a49c4256
Sunil <srao0194@protonmail.com>
2020-06-28 10:26:29 -0500

    revert

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -9,8 +9,13 @@ set -e -u -x
 #  ln -s /run/secrets/private_rsa_key ~/.ssh/id_rsa
 #  ln -s /run/secrets/public_rsa_key ~/.ssh/id_rsa.pub
 
+<<<<<<< HEAD
 #  cat ~/.ssh/id_rsa
 #  cat ~/.ssh/id_rsa.pub
+=======
+  echo ~/.ssh/id_rsa
+  echo ~/.ssh/id_rsa.pub
+>>>>>>> parent of 4ef1054... sym link.... solved all the problems
 #
 #  touch /root/.ssh/known_hosts
 #  chmod 644 ~/.ssh/known_hosts
