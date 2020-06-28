ca33bbe848f1231ca1c52585d942372584a9a883
Sunil <srao0194@protonmail.com>
2020-06-28 18:20:26 -0500

    updated integration test shell script

--- ci-cd/lakota-ed/tasks/integration-test/run.sh
+++ ci-cd/lakota-ed/tasks/integration-test/run.sh
@@ -10,6 +10,7 @@ ls -la
 # This is where you can run your unit test command, such as the ones below:
 echo "Running integration tests against QA..."
 
+apt-get install libgtk2.0-0 libgtk-3-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
 npm install cypress --save-dev
 CYPRESS_BASE_URL=http://lakota.skeletonpraxis.net/ node_modules/.bin/cypress run
 
