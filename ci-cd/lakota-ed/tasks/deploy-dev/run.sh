9113eb01ebc5b1decde515001a4fa0bcfd80ed34
Sunil <srao0194@protonmail.com>
2020-06-28 14:08:29 -0500

    fix conflicts

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -2,29 +2,29 @@
 
 set -e -u -x
 
-{
-  mkdir -p /root/.ssh
-  chmod 0700 ~/.ssh
+#{
+#  mkdir -p /root/.ssh
+#  chmod 0700 ~/.ssh
 #
-  chmod 600 /run/secrets/private_rsa_key
-  chmod 644 /run/secrets/public_rsa_key
-
-  ln -s /run/secrets/private_rsa_key /root/.ssh/id_rsa
-  ln -s /run/secrets/public_rsa_key /root/.ssh/id_rsa.pub
+#  chmod 600 /run/secrets/private_rsa_key
+#  chmod 644 /run/secrets/public_rsa_key
+#
+#  ln -s /run/secrets/private_rsa_key /root/.ssh/id_rsa
+#  ln -s /run/secrets/public_rsa_key /root/.ssh/id_rsa.pub
 
 #  cat ~/.ssh/id_rsa
 #  cat ~/.ssh/id_rsa.pub
 #
-  touch /root/.ssh/known_hosts
-  chmod 644 ~/.ssh/known_hosts
+#  touch /root/.ssh/known_hosts
+#  chmod 644 ~/.ssh/known_hosts
 
 #
-  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
+#  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
 #  ssh-keyscan -H 134.122.124.158 >> /root/.ssh/known_hosts
 
 #  cat /run/secrets/private_rsa_key
 #  cat /run/secrets/public_rsa_key
-}
+#}
 ls -la ~/.ssh
 cd dev-lakota-ed
 #npm install
