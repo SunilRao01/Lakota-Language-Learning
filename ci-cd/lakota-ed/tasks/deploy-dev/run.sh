4bd1e21dcec9581c7d25ae026411e395c59264cb
Sunil <srao0194@protonmail.com>
2020-06-28 10:28:48 -0500

    fix conflicts

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -2,27 +2,22 @@
 
 set -e -u -x
 
-#{
-#  mkdir -p /root/.ssh
-#  chmod 0700 ~/.ssh
-#
-#  ln -s /run/secrets/private_rsa_key ~/.ssh/id_rsa
-#  ln -s /run/secrets/public_rsa_key ~/.ssh/id_rsa.pub
+{
+  mkdir -p /root/.ssh
+  chmod 0700 ~/.ssh
 
-<<<<<<< HEAD
-#  cat ~/.ssh/id_rsa
-#  cat ~/.ssh/id_rsa.pub
-=======
-  echo ~/.ssh/id_rsa
-  echo ~/.ssh/id_rsa.pub
->>>>>>> parent of 4ef1054... sym link.... solved all the problems
+  ln -s /run/secrets/private_rsa_key ~/.ssh/id_rsa
+  ln -s /run/secrets/public_rsa_key ~/.ssh/id_rsa.pub
+
+  cat ~/.ssh/id_rsa
+  cat ~/.ssh/id_rsa.pub
 #
 #  touch /root/.ssh/known_hosts
 #  chmod 644 ~/.ssh/known_hosts
 #
 #  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
 #  ssh-keyscan -H 134.122.124.158 >> /root/.ssh/known_hosts
-#}
+}
 
 cd dev-lakota-ed
 #npm install
