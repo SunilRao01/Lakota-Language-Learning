dee2fe41e20360668eef7e580c951b9506cbff4e
Sunil <srao0194@protonmail.com>
2020-06-28 06:19:26 -0500

    test dev

--- ci-cd/lakota-ed/tasks/deploy-dev/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-dev/run.sh
@@ -3,37 +3,17 @@
 set -e -u -x
 
 {
-    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-#    ssh-keygen -f ~/.ssh/id_rsa_private -t rsa -N ''
+  mkdir /root/.ssh
+  chmod 700 ~/.ssh
 
-    rsa=${RSA_KEY}
-    echo "$rsa" > ~/.ssh/id_rsa_private
+  rsa_private=${PRIVATE_RSA_KEY}
+  rsa_public=${PUBLIC_RSA_KEY}
 
-    touch ~/.ssh/config
-    echo $'Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_rsa_private\n' > ~/.ssh/config
+  echo "$rsa_private" > ~/.ssh/id_rsa
+  echo "$rsa_public" > ~/.ssh/id_rsa.pub
 
-    ls ~/.ssh
-    cat ~/.ssh/config
-
-    ssh-keygen -f ~/.ssh/id_rsa_private -t rsa -N ''
-
-    ls ~/.ssh
-#    echo
-#
-#    ssh-keygen -t rsa -N "" -f rsa.key
-#    echo "$rsa" > ./rsa.key
-#    mkdir /root/.ssh
-#    cp ./rsa.key ~/.ssh/id_rsa
-#    rm rsa.key
-#    rm rsa.key.pub
-#
-#    touch ~/.ssh/id_rsa.pub
-#    chmod 644 ~/.ssh/id_rsa.pub
-#
-#    ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-
-    ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
-    ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
+  ssh-keyscan -H skeletonpraxis.net >> ~/.ssh/known_hosts
+  ssh-keyscan -H 134.122.124.158 >> ~/.ssh/known_hosts
 }
 
 cd dev-lakota-ed
