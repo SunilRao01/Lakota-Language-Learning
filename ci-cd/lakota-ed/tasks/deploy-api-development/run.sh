42d2b95255b56eeb5c5166b28a35f1aea6424795
Sunil <srao0194@protonmail.com>
2020-07-12 12:09:43 -0500

    fixed api development step (wip)

--- ci-cd/lakota-ed/tasks/deploy-api-development/run.sh
+++ ci-cd/lakota-ed/tasks/deploy-api-development/run.sh
@@ -2,5 +2,20 @@
 
 set -e -u -x
 
-pwd
-ls
+{
+  private_rsa_key=${PRIVATE_RSA_KEY}
+  public_rsa_key=${PUBLIC_RSA_KEY}
+
+  ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+  echo "$private_rsa_key" > ~/.ssh/id_rsa
+  echo "$public_rsa_key" > ~/.ssh/id_rsa.pub
+
+  touch /root/.ssh/known_hosts
+  chmod 644 ~/.ssh/known_hosts
+
+  ssh-keyscan -H skeletonpraxis.net >> /root/.ssh/known_hosts
+}
+
+#cd dev-lakota-ed
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@skeletonpraxis.net "cd /root/src/Lakota-Language-Learning/ && git status"
