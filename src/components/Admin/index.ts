acc953c83e8404e0a42118d8be8200f3af2f2f3f
Sunil <srao0194@protonmail.com>
2020-09-26 17:36:18 -0500

    cleaned up import in root App.component.tsx

--- src/components/Admin/index.ts
+++ src/components/Admin/index.ts
@@ -1,6 +1,7 @@
 export {default as AdminHome} from './AdminHome';
 export {default as AdminLessons} from './AdminLessons';
 export {default as AdminGrammar} from './AdminGrammar';
+export {default as AdminVocabulary} from './AdminVocabulary';
 export {default as AdminLogin} from './AdminLogin';
 export {default as AdminPostCreate} from './AdminPostCreate'
 export {default as AdminPostEdit} from './AdminPostEdit'
