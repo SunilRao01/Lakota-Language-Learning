e59cfba6393291f2d8b980f4ffd9740c0cf00e43
Sunil <srao0194@protonmail.com>
2021-03-17 20:46:36 -0500

    lint, clean up

--- src/components/FilteredPostsView/FilteredPostsView.component.tsx
+++ src/components/FilteredPostsView/FilteredPostsView.component.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import { connect } from 'react-redux';
 import { Post } from 'redux/Posts/Posts.reducer';
 import qs from 'query-string';
@@ -24,72 +24,96 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
         setPostLoading,
     } = props;
 
-    const [selectedCategoryFilters, setSelectedCategoryFilters] = useState<string[]>([])
-    const [selectedTagFilters, setSelectedTagFilters] = useState<string[]>([])
+    const [selectedCategoryFilters, setSelectedCategoryFilters] = useState<
+        string[]
+    >([]);
+    const [selectedTagFilters, setSelectedTagFilters] = useState<string[]>([]);
     const [currentPage, setCurrentPage] = useState(1);
 
-    const fetchData = useCallback(async () => {
-        setPostLoading(true);
-        clearPosts();
-        await getPostsByFilter(currentPage, selectedCategoryFilters, selectedTagFilters);
-        setPostLoading(false);
+    // Memoized value of tags parsed from the URL
+    const tagsFromUrl = useMemo<string[] | undefined>(() => {
+        const queryStrings = qs.parse(history.location.search);
+
+        let tags: any = queryStrings['tag'];
+        if (tags) {
+            if (typeof tags === 'string' && tags.length > 0) {
+                return [tags];
+            } else if (Array.isArray(tags)) {
+                return tags;
+            }
+        }
+
+        return undefined;
+    }, [history.location.search]);
+
+    // Memoized value of categories parsed from the URL
+    const categoriesFromUrl = useMemo<string[] | undefined>(() => {
+        const queryStrings = qs.parse(history.location.search);
+
+        let categories: any = queryStrings['category'];
+        if (categories) {
+            if (typeof categories === 'string' && categories.length > 0) {
+                return [categories];
+            } else if (Array.isArray(categories)) {
+                return categories;
+            }
+        }
+
+        return undefined;
+    }, [history.location.search]);
+
+    // Memoized value of page parsed from the URL
+    const pageFromUrl = useMemo<number | undefined>(() => {
+        const queryStrings = qs.parse(history.location.search);
+
+        let page: any = queryStrings['page'];
+        if (page) {
+            return +page;
+        }
+
+        return undefined;
+    }, [history.location.search]);
+
+    useEffect(() => {
+        categoriesFromUrl && setSelectedCategoryFilters(categoriesFromUrl);
+    }, [categoriesFromUrl]);
+
+    useEffect(() => {
+        tagsFromUrl && setSelectedTagFilters(tagsFromUrl);
+    }, [tagsFromUrl]);
+
+    useEffect(() => {
+        pageFromUrl
+            ? setCurrentPage(pageFromUrl)
+            : history.replace(`${location.pathname}${location.search}&page=1`);
+    }, [history, location.pathname, location.search, pageFromUrl]);
+
+    useEffect(() => {}, [currentPage]);
+
+    useEffect(() => {
+        const getPostsForCategoriesAndTags = async (
+            categories: string[],
+            tags: string[]
+        ) => {
+            setPostLoading(true);
+            clearPosts();
+            await getPostsByFilter(currentPage, categories, tags);
+            setPostLoading(false);
+        };
+
+        (selectedCategoryFilters.length > 0 || selectedTagFilters.length > 0) &&
+            getPostsForCategoriesAndTags(
+                selectedCategoryFilters,
+                selectedTagFilters
+            );
     }, [
-        selectedCategoryFilters,
         clearPosts,
         currentPage,
         getPostsByFilter,
-        setPostLoading,
+        selectedCategoryFilters,
         selectedTagFilters,
+        setPostLoading,
     ]);
-
-    // Memoized value of tags parsed from the URL
-    // const tagsFromurl = useMemo<string[] | undefined>(() => {
-    //     const queryStrings = qs.parse(history.location.search);
-    //
-    //     let tags: any = queryStrings['tag'];
-    //     if (tags) {
-    //         if (typeof tags === 'string' && tags.length > 0) {
-    //             return [tags]
-    //         } else if (Array.isArray(tags)) {
-    //             return tags;
-    //         }
-    //     }
-    //
-    //     return undefined;
-    // }, [history.location.search])
-    //
-    // // Memoized value of categories parsed from the URL
-    // const categoriesFromUrl = useMemo<string[] | undefined>(() => {
-    //     const queryStrings = qs.parse(history.location.search);
-    //
-    //     let tags: any = queryStrings['tag'];
-    //     if (tags) {
-    //         if (typeof tags === 'string' && tags.length > 0) {
-    //             return [tags]
-    //         } else if (Array.isArray(tags)) {
-    //             return tags;
-    //         }
-    //     }
-    //
-    //     return undefined;
-    // }, [history.location.search])
-    //
-    // // Memoized value of page parsed from the URL
-    // const pageFromUrl = useMemo<number | undefined>(() => {
-    //     const pageSearchIndex = history.location.search.indexOf('page') + 5;
-    //     if (pageSearchIndex) {
-    //         const page = history.location.search.substr(pageSearchIndex);
-    //         return +page;
-    //     }
-    //
-    //     return undefined;
-    // }, [history.location.search]);
-
-    useEffect(() => {
-        if (selectedTagFilters.length > 0 || selectedTagFilters.length > 0) {
-            fetchData();
-        }
-    }, [selectedCategoryFilters.length, fetchData, selectedTagFilters.length]);
 
     const addTagFilter = (tag: string) => {
         if (!selectedTagFilters.includes(tag)) {
@@ -113,29 +137,57 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
         }
     };
 
-    // TODO:
+    const parsedUrlFromFilters = useMemo(() => {
+        let searchQuery: string = '';
+
+        // Add categories to URL
+        for (let i = 0; i < selectedCategoryFilters.length; i++) {
+            const category = selectedCategoryFilters[i];
+
+            if (i === 0) {
+                searchQuery += `?category=${category}`;
+            } else {
+                searchQuery += `&category=${category}`;
+            }
+        }
+
+        // Add tags to URL
+        for (let i = 0; i < selectedTagFilters.length; i++) {
+            const tag = selectedTagFilters[i];
+
+            if (i === 0 && selectedCategoryFilters.length === 0) {
+                searchQuery += `?tag=${tag}`;
+            } else {
+                searchQuery += `&tag=${tag}`;
+            }
+        }
+
+        return searchQuery
+    }, [selectedTagFilters, selectedCategoryFilters])
+
     const onNextPage = useCallback(() => {
         if (posts.length !== 0) {
-            setCurrentPage(currentPage + 1);
+            // setCurrentPage(currentPage + 1);
 
             history.push({
-                search: `?category=${selectedCategoryFilters.length ? : ''}&page=${currentPage + 1}`,
+                search: `${parsedUrlFromFilters}&page=${currentPage + 1}`,
             });
         }
-    }, [currentPage, history, posts.length, selectedGrammar]);
+    }, [currentPage, history, parsedUrlFromFilters, posts.length]);
+
     const onPreviousPage = useCallback(() => {
         if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
+            // setCurrentPage(currentPage - 1);
 
             history.push({
-                search: `?category=${selectedGrammar}&page=${currentPage - 1}`,
+                search: `${parsedUrlFromFilters}&page=${currentPage - 1}`,
             });
         }
-    }, [currentPage, history, selectedGrammar]);
+    }, [currentPage, history, parsedUrlFromFilters]);
 
     const disableNextPage = useMemo(
-      () => posts.length === 0 || posts.length < 5,
-      [posts]
+        () => posts.length === 0 || posts.length < 5,
+        [posts]
     );
     const disablePreviousPage = useMemo(() => currentPage === 1, [currentPage]);
 
@@ -145,7 +197,8 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
                 <div className="is-size-3 title">Tags:</div>
                 {
                     <div className="tags are-large">
-                        {!selectedTagFilters || selectedTagFilters.length === 0 ? (
+                        {!selectedTagFilters ||
+                        selectedTagFilters.length === 0 ? (
                             <div>No Tag Filters</div>
                         ) : (
                             selectedTagFilters.map((_, i: number) => (
@@ -158,21 +211,25 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
                 <div className="is-size-3 title">Categories:</div>
                 {
                     <div className="tags are-large">
-                        {!selectedCategoryFilters || selectedCategoryFilters.length === 0 ? (
+                        {!selectedCategoryFilters ||
+                        selectedCategoryFilters.length === 0 ? (
                             <div>No Category Filters</div>
                         ) : (
-                            selectedCategoryFilters.map((c: string, i: number) => (
-                                <div key={i}>
-                                    <Link
-                                        to={`/posts?categories=${c}`}
-                                    >{`${c}`}</Link>
-                                    {`${
-                                        i < selectedCategoryFilters.length - 1
-                                            ? `, `
-                                            : ``
-                                    }`}
-                                </div>
-                            ))
+                            selectedCategoryFilters.map(
+                                (c: string, i: number) => (
+                                    <div key={i}>
+                                        <Link
+                                            to={`/posts?category=${c}`}
+                                        >{`${c}`}</Link>
+                                        {`${
+                                            i <
+                                            selectedCategoryFilters.length - 1
+                                                ? `, `
+                                                : ``
+                                        }`}
+                                    </div>
+                                )
+                            )
                         )}
                     </div>
                 }
@@ -201,18 +258,8 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
             {!postsLoading && (
                 <button
                     className="button is-info pagination-button"
-                    disabled={currentPage === 1}
-                    onClick={() => {
-                        if (currentPage > 1) {
-                            setCurrentPage(currentPage - 1);
-
-                            getPostsByFilter(
-                                currentPage - 1,
-                                selectedCategoryFilters,
-                                selectedTagFilters
-                            );
-                        }
-                    }}
+                    disabled={disablePreviousPage}
+                    onClick={onPreviousPage}
                 >
                     Previous Page
                 </button>
@@ -220,18 +267,8 @@ export const FilteredPostsView: FC<FilteredPostsViewPropsAndActions> = (
             {!postsLoading && (
                 <button
                     className="button is-info pagination-button"
-                    disabled={posts.length === 0 || posts.length < 5}
-                    onClick={() => {
-                        if (posts.length !== 0) {
-                            setCurrentPage(currentPage + 1);
-
-                            getPostsByFilter(
-                                currentPage + 1,
-                                selectedCategoryFilters,
-                                selectedTagFilters
-                            );
-                        }
-                    }}
+                    disabled={disableNextPage}
+                    onClick={onNextPage}
                 >
                     Next Page
                 </button>
