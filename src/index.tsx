fe575701ee439b63e3a9222d30e0cc3bc66ed040
Sunil <srao0194@protonmail.com>
2021-03-21 19:35:19 -0500

    added chevron feather icons, updated Grammar.component.tsx to use dropdown instead of width tab selection, updated package-lock.json

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from 'components/App';
 import { store } from 'redux/store';
-
 import './index.css';
 
 render(
